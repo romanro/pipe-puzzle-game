90140dabade7c57e606368446310162256a0fb02
Roman Rozanov <antifreez@gmail.com>
2021-11-13 23:38:02 +0200

    basic error handeling added to socket provider

--- src/providers/SocketProvider/SocketProvider.tsx
+++ src/providers/SocketProvider/SocketProvider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, createContext } from 'react';
+import { useEffect, useState, createContext, FC } from 'react';
 import { ISocketProvider } from './SocketProvider.models';
 import { SOCKET_SETTINGS } from 'utils/settings';
 
@@ -7,18 +7,27 @@ const webSocket = new WebSocket(SOCKET_URL);
 
 export const SocketContext = createContext<WebSocket>(webSocket);
 
-export const SocketProvider = (props: ISocketProvider) => {
+export const SocketProvider: FC<ISocketProvider> = ({ children }: ISocketProvider) => {
     const [ws, setWs] = useState<WebSocket>(webSocket);
     useEffect(() => {
         const onClose = () => {
+            console.warn(`Websocket was disconnected!!! Trying to reconnect in ${SOCKET_RECONNECTION_TIMEOUT} ms`);
             setTimeout(() => {
                 setWs(new WebSocket(SOCKET_URL));
             }, SOCKET_RECONNECTION_TIMEOUT);
         };
+
+        const onError = () => {
+            console.error('There was an error in Websocket');
+            window.alert('There was an error in Websocket');
+        };
+
         ws.addEventListener('close', onClose);
+        ws.addEventListener('error', onError);
         return () => {
             ws.removeEventListener('close', onClose);
+            ws.removeEventListener('error', onError);
         };
     }, [ws, setWs]);
-    return <SocketContext.Provider value={ws}>{props.children}</SocketContext.Provider>;
+    return <SocketContext.Provider value={ws}>{children}</SocketContext.Provider>;
 };
